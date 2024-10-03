f2781ed60b18aa14a3ece63a600bd07769403eb5
Monixc <chris13404@gmail.com>
2024-10-04 06:22:44 +0900

    file: 폴더 수정

--- src/app/(user)/home/[id]/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function DetailPage() {
-  return <main>diary</main>;
-}
--- src/app/(user)/home/page.tsx
+++ src/app/(user)/home/page.tsx
@@ -1,3 +1,3 @@
 export default function HomePage() {
-  return <main>diary</main>;
+  return <main>home</main>;
 }
